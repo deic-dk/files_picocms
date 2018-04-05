471ffceb84af86418b2e00a9f5dd8575765665e5
deltafunction <frederik@orellana.dk>
2018-04-05 23:09:24 +0200

    Localization.

--- l10n/da.php
+++ l10n/da.php
@@ -2,5 +2,23 @@
 $TRANSLATIONS = array(
 		"Choose" => "Vælg",
 		"Cancel" => "Fortryd",
+		"Site folders" => "Web-mapper",
+		"More info" => "Mere info",
+		"Site folders must contain files in markdown format - with extension 'md'.
+	These files will be parsed and served up to browsers as html by Pico CMS.
+	Click on the plus below to add a folder. You must youself create a \"content\"
+	subfolder inside this folder and populate it with pages in Markdown format -
+	plus a \"themes\" subfolder if you want to customize your pages.
+	A site will be served at the URL %s/sites/site_folder, where
+	\"site_folder\" is the name of site folder you choose." =>
+		"Web-mapper skal indeholde filer i markdown format - med extension 'md'.
+	Disse filer vil blive parsede og serveret til browsere som html af Pico CMS.
+	Klik på plus nedenfor for at tilføje en web-mappe. Du skal selv lave en
+	\"content\"-undermappe og fylde den med markdown-filer -
+	plus en \"themes\"-undermappe hvis du vil tipasse dine siders layout m.m.
+	En web-mappe vil blive serveret som et web-site på URL'en %s/sites/web_mappe, hvor
+	\"web_mappe\" er navnet på web-mappen.",
+		"Or use the website wizard to create a simple site." => "Eller vælg website-wizarden for at skabe et simpelt site.",
+		"Website wizard" => "Website-guide",
 );
 $PLURAL_FORMS = "nplurals=2; plural=(n != 1);";
--- templates/personalsettings.php
+++ templates/personalsettings.php
@@ -1,18 +1,18 @@
 <fieldset id="filesPicoSiteFolders" class="section">
-	<a href="<?php print($_['samplesite_url']);?>" class="right-info-link">More info &raquo;</a>
-	<h2><?php p($l->t('Site folders')); ?></h2>
-	Site folders must contain files in markdown format - with extension 'md'.
+	<a href="<?php print_unescaped($_['samplesite_url']);?>" class="right-info-link"><?php p($l->t('More info'));?> &raquo;</a>
+	<?php print_unescaped("<h2>".$l->t('Site folders')."</h2>".
+	$l->t("Site folders must contain files in markdown format - with extension 'md'.
 	These files will be parsed and served up to browsers as html by Pico CMS.
-	Click on the plus below to add a folder. You must youself create a "content"
+	Click on the plus below to add a folder. You must youself create a \"content\"
 	subfolder inside this folder and populate it with pages in Markdown format -
-	plus a "themes" subfolder if you want to customize your pages.
-	A site will be served at the URL <?php print(OC::$WEBROOT);?>/sites/site_folder, where
-	"site_folder" is the name of site folder you choose.
+	plus a \"themes\" subfolder if you want to customize your pages.
+	A site will be served at the URL %s/sites/site_folder, where
+	\"site_folder\" is the name of site folder you choose.", array(OC::$WEBROOT)));?>
 	<br /><br />
-	Or use the website wizard to create a simple site.	
+	<?php p($l->t("Or use the website wizard to create a simple site."));?>
 	<br /><br />
 	<div style="text-align: center;"><div  class="edit_personal_website btn btn-primary btn-flat">
-	Website wizard</div></div>
+	<?php p($l->t("Website wizard"));?></div></div>
 	<br />
 	<div id="filesPicoSiteFoldersList">
 	<?php foreach($_['site_folders'] as $p){
